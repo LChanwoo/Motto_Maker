4a3902dd131c3285da9059e0596f20013996fb0a
NEST\sblm7 <chw210601@gmail.com>
2022-09-28 19:15:02 +0900

    no message

--- src/index.tsx
+++ src/index.tsx
@@ -15,10 +15,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  // <App />
-  <TMP />
-  // </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
